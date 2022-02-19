639b8337a88ee52f1833e6916b6e590d3a762a34
Divyank <divyankrawat10@gmail.com>
2022-02-19 14:19:03 +0530

    installing dependencies and building base layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 
 function App() {
   return (
-    <>
-      creating CryptoVerse. A full fledged Crypto info. platfrom
-    </>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path='/' element={<Homepage />} />
+              <Route path='/exchanges' element={<Exchanges />} />
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+              <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+              <Route path='/news' element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
   );
 }
 
