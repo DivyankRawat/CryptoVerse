81ed3380f9a533767a405e9400c0e3ed1756024f
Divyank <divyankrawat10@gmail.com>
2022-02-20 13:12:01 +0530

    News Section Completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { cryptoApi } from '../services/cryptoApi'
+import { cryptoNewsApi } from '../services/cryptoNewsApi'
 
 export const store = configureStore({
     // Added crypto api in store
-    reducer: { [cryptoApi.reducerPath]: cryptoApi.reducer },
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
+
+    },
 })
