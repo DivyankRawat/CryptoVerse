bfc1e5ea2c0b12b55bf4f5a65122573f58cbcd73
Divyank <divyankrawat10@gmail.com>
2022-02-21 17:28:21 +0530

    Rebuild 2.0

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
 
-function CryptoDetails() {
-  return (
-    <div>CryptoDetails</div>
-  )
-}
+const { Title, Text } = Typography;
+const { Option } = Select;
 
-export default CryptoDetails
+const CryptoDetails = () => {
+  const { coinId } = useParams();
+  return <div>CryptoDetails {coinId}</div>;
+};
+
+export default CryptoDetails;
