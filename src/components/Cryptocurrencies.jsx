ade8e8c5b532c80f7776861bd210245e3200f0e0
Divyank <divyankrawat10@gmail.com>
2022-02-21 11:14:46 +0530

    Rebuilding

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,48 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import millify from 'millify' 
-import {Card, Row, Col, Input} from 'antd'
-import {Link} from 'react-router-dom'
-import { useGetCryptosQuery } from '../services/cryptoApi'
+import React from 'react'
 
-function Cryptocurrencies({simplified}) {
-  // If the count is simplified, show 10 else show 100. Simplied is taken as prop as it is already used in homepage..
-  const count = simplified ? 10: 100;
-  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
-  const[searchTerm, setSearchTerm] = useState('');
-  // console.log(cryptos); will display all the coins
-  
-  useEffect(()=>{
-    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    setCryptos(filteredData);
-  }, [cryptosList, searchTerm])
-  
-  if(isFetching) return 'Loading...'
-
+function Cryptocurrencies() {
   return (
-    <div>
-      {!simplified && (
-      <div className='search-crypto' >
-        <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)}/>
-      </div>
-      )
-      }
-        <Row gutter={[32,32]} className='crypto-card-container'>
-        {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id} >
-            <Link to={`/crypto/${currency.id}`}>
-                <Card title={`${currency.rank}. ${currency.name}`} extra={<img className='crypto-image' src={currency.iconUrl}/>} 
-                  hoverable
-                  >
-                  <p>Price : {millify(currency.price)}</p>
-                  <p>Market Cap : {millify(currency.marketCap)}</p>
-                  <p>Daily Change : {millify(currency.change)}</p>
-            </Card>
-            </Link>
-          </Col>
-        ))}
-        </Row>
-    </div>
+    <div>Cryptocurrencies</div>
   )
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,41 +1,8 @@
 import React from 'react'
-import millify from 'millify'  // use to overall a particul number which is too big to display ex. 1M
-import {Typography , Row, Col, Statistic} from 'antd'
-import {Link} from 'react-router-dom'
-import { useGetCryptosQuery } from '../services/cryptoApi'
-import Cryptocurrencies from '../components/Cryptocurrencies'
-import News from '../components/News'
-
-const { Title } = Typography;
 
 function Homepage() {
-
-  const { data, isFetching } = useGetCryptosQuery(10);
-  // console.log(data);
-  if(isFetching) return 'Loading...'
-  const GlobalStats = data?.data?.stats;
-
   return (
-    <div>
-      <Title level={2} className='heading'>Global Crypto Stats</Title>
-      <Row>
-        <Col span={12}><Statistic title='Total Cryptocurrencies' value={GlobalStats.total}></Statistic></Col>
-        <Col span={12}><Statistic title='Total Total Exchanges' value={millify(GlobalStats.totalExchanges)}></Statistic></Col>
-        <Col span={12}><Statistic title='Total Market Cap' value={millify(GlobalStats.totalMarketCap)}></Statistic></Col>
-        <Col span={12}><Statistic title='Total 24h Volume' value={millify(GlobalStats.total24hVolume)}></Statistic></Col>
-        <Col span={12}><Statistic title='Total Markets' value={millify(GlobalStats.totalMarkets)}></Statistic></Col>
-      </Row>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
-        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
-      </div>
-      <Cryptocurrencies simplified />
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">Latest Crypto News</Title>
-        <Title level={3}><Link to="/news">Show more</Link></Title>
-      </div>
-      <News simplified />
-    </div>
+    <div>Homepage</div>
   )
 }
 
